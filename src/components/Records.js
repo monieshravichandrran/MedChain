3c0f1767b11dc0d25165118c5dc7c8c52b449d0d
monieshravichandrran <monieshravichandrran@gmail.com>
2022-10-12 08:54:25 +0530

    commit changes pushed

--- src/components/Records.js
+++ src/components/Records.js
@@ -3,8 +3,55 @@ import FullPageLoader from "./FullPageLoader";
 
 const Records = (props) => {
   const [loader,setLoader] = useState(false);
-  const downloadRecord = () =>{
-
+  const downloadRecord = async() =>{
+    setLoader(true);
+    // acc = [];
+    // acc1 = [];
+    // const fn = async () => {
+    //     const db = getFirestore();
+    //     const usersRef = collection(db, "users");
+    //     const q = query(
+    //         usersRef,
+    //         where("email", "==", props.grants.data.from)
+    //     );
+    //     const querySnapshot = await getDocs(q);
+    //     querySnapshot.forEach((doc) => {
+    //         acc.push({
+    //             id: doc.id,
+    //             data: doc.data(),
+    //         });
+    //     });
+    // };
+    // const fn1 = async () => {
+    //     const db1 = getFirestore();
+    //     const usersRef1 = collection(db1, "users");
+    //     const q1 = query(
+    //         usersRef1,
+    //         where("email", "==", props.grants.data.patient)
+    //     );
+    //     const querySnapshot1 = await getDocs(q1);
+    //     querySnapshot1.forEach((doc1) => {
+    //         acc1.push({
+    //             id: doc1.id,
+    //             data: doc1.data(),
+    //         });
+    //     });
+    // };
+    // await fn();
+    // await fn1();
+    // let asciiArray = await contract.methods
+    //     .getDS(acc1[0].data.address, acc[0].data.address)
+    //     .call({ from: accounts[0] });
+    // console.log(acc, accounts, asciiArray);
+    // const cid = convertToString(asciiArray);
+    // const url = `https://ipfs.io/ipfs/${cid}`;
+    // const link = document.createElement("a");
+    // link.href = url;
+    // link.target = "_blank";
+    // link.setAttribute("download", "file.pdf");
+    // document.body.appendChild(link);
+    // link.click();
+    // setLoader(false);
   }
 
   return (
